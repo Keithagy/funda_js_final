8327b1dfc14a6cff853e3da9236fa3d51d989c8e
Keithagy <68194558+Keithagy@users.noreply.github.com>
2021-08-19 16:27:47 +0800

    Comments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+// App state
 const LASTPAGE = 2475;
 const FIRSTPAGE = 1;
 
 let firstComicDisplayed = 1;
 let comicsOnPage = 3;
 
+// First Render (see domManagement.js)
 addFormSubmitHandlers();
-executeRenderTick(firstComicDisplayed, comicsOnPage);
+executeRenderTick(firstComicDisplayed, comicsOnPage); 
