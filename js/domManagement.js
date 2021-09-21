0bf95224316bd1f66da3d35bea9431fb29bba49e
Keithagy <68194558+Keithagy@users.noreply.github.com>
2021-09-21 11:01:25 +0800

    commented in module-based organization

--- js/domManagement.js
+++ js/domManagement.js
@@ -28,3 +28,39 @@ const addFormSubmitHandlers = () => {
     const skipForm = document.querySelector(".skipToComicForm");
     skipForm.addEventListener("submit", handleComicSkip);
 };
+
+// // If we used modules instead...
+// const domManagement = (() => {
+//     // Called in executeRenderTick, prior to beginning API call (async)
+// const showLoading = () => {
+//     const comicStrip = document.querySelector(".comicStrip");
+//     comicStrip.innerHTML = "Loading...";
+// };
+
+// // Called in executeRenderTick, following API call (following successful rendering of comics)
+// const bindFormValues = () => {
+//     const comicsOnPageForm = document.querySelector("#comicsOnPage");
+//     comicsOnPageForm.value = comicsOnPage;
+
+//     const skipToComicControl = document.querySelector(".skipToComicControl");
+//     skipToComicControl.value = firstComicDisplayed;
+// };
+
+// // Called in main.js and event handlers, refreshes interface to reflect state
+// const executeRenderTick = (firstComicDisplayed, comicsOnPage) => {
+//     showLoading();
+//     renderInOrder(firstComicDisplayed, comicsOnPage); // See fetchRenderComics.js
+//     bindFormValues();
+// };
+
+// // Called in main.js (first render of page)
+// const addFormSubmitHandlers = () => {
+//     const resizeForm = document.querySelector(".pageSizeControl");
+//     resizeForm.addEventListener("submit", handleResizePage);
+
+//     const skipForm = document.querySelector(".skipToComicForm");
+//     skipForm.addEventListener("submit", handleComicSkip);
+// };
+
+// return {addFormSubmitHandlers,executeRenderTick}
+// })()
